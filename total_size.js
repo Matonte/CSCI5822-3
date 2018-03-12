33f860934aec42dcd7f6ffb54709712d0d451e1e
Matt Weber <mawe6437@colorado.edu>
2018-03-12 17:43:40 -0600

    Updated error handling, spacing

--- total_size.js
+++ total_size.js
@@ -1,52 +1,92 @@
+// CSCSI 5828 - Spring 2018
+// Homework #3
+// Matt Weber, Michael Matonte
+
 var fs = require('fs'),
-    path = require('path'),
-    a = require('async');
+path = require('path'),
+async = require('async');
 
-
-function readSizeRecursive(item, cb) {
-    fs.lstat(item, function(err, stats) {
-        if (!err && stats.isDirectory()) {
+function readSizeRecursive(item, cb)
+{
+    fs.lstat(item, function(err, stats)
+    {
+        if (!err && stats.isDirectory())
+        {
             var total = stats.size;
 
-            fs.readdir(item, function(err, list) {
+            fs.readdir(item, function(err, list)
+            {
                 if (err) return cb(err);
 
-                async.forEach(
-                    list,
-                    function(diritem, callback) {
-                        readSizeRecursive(path.join(item, diritem), function(err, size) {
-                            total += size;
-                            callback(err);
-                        });
-                    },
-                    function(err) {
-                        cb(err, total);
-                    }
-                );
+                async.forEach( list, function(diritem, callback)
+                {
+                    readSizeRecursive(path.join(item, diritem), function(err, size)
+                    {
+                        total += size;
+                        callback(err);
+                    });
+                },
+                function(err)
+                {
+                    cb(err, total);
+                });
             });
-        } else {
-            if (err){
-				cb(err);
-			}
-			else {
-				cb(null,stats.size);
-			}
-			
+        }
+        else
+        {
+            if (err)
+            {
+                cb(err);
+            }
+            else
+            {
+                cb(null,stats.size);
+            }
         }
     });
 }
-function findSize(arg) {
-	readSizeRecursive(arg, (err, size) => {
-		console.log(size);
 
-	});
+function findSize(arg)
+{
+    readSizeRecursive(arg, (err, size) =>
+    {
+        if(err)
+        {
+            console.log("Error: ",err);
+        }
+        else
+        {
+            console.log("Total size: ",size);
+        }
+    });
 }
 
 var args = process.argv.slice(2);
 console.log("args "+ args );
 
-if (args[0]) {
-	console.log(args[0]);
-	console.log(findSize(args[0]));
+if (args[0])
+{
+    fs.lstat(args[0], function(err, stat)
+    {
+        if(err)
+        {
+            console.log("Error: ",err);
+        }
+        else
+        {
+            if (stat.isDirectory())
+            {
+                console.log("Checking dir: ", args[0]);
+                findSize(args[0]);
+            }
+            else
+            {
+                console.log("Error: ",args[0], " is not a dir!");
+            }
+        }
+    });
 } 
-else console.err("Usage: total_size <path>");
+else
+{
+    console.log("Error: No arg provided");
+}
